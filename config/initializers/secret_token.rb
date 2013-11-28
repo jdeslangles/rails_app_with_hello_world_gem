5a72bf20e0f11d0a2f9d433ca56fd152b7c38962
jdeslangles <julien.deslangles@gmail.com>
2013-11-28 10:27:39 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyHelloGem::Application.config.secret_token = 'a30fa5ec51a2f74ead28669ca0f7f48676708719fc2d692028bd31148294537d1c8c3cc281c6c4e608d26259921f856e980f87665f0a041d6934e56f1d90f4a1'
